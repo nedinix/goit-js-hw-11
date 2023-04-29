6009fd775e4b28938fdab5d698f433052aea876b
nedinix <113529703+nedinix@users.noreply.github.com>
2023-04-29 13:37:57 +0000

    Deploying to gh-pages from @ nedinix/goit-js-hw-11@82d1ec32b0514f821b18b3a12c77acb296456c28 🚀

--- index.dbcb16ac.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={searchForm:document.querySelector(".seatch-form")};console.log(o)}();
-//# sourceMappingURL=index.dbcb16ac.js.map
--- index.f8d9d770.js
+++
@@ -1,2 +0,0 @@
-const o={searchForm:document.querySelector(".seatch-form")};console.log(o);
-//# sourceMappingURL=index.f8d9d770.js.map
